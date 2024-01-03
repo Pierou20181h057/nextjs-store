b0fa5612e9db046b439652f889f0618e97d41b21
Piero Liberato <54274115+Pierou20181h057@users.noreply.github.com>
2024-01-03 09:11:39 -0500

    Add: CSS Modules in Next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { Description } from "app/components/home/Description"
-
+import { Hero } from "app/components/home/Hero"
+import { MainProducts } from "app/components/home/MainProducts/MainProducts"
 //"use client" pARA USAR EL CONSOLA DEL SERVIDOR
 
 export default function Home() {
   return (
     <main>
+      <Hero/>
       <Description/>
+      <MainProducts/>
     </main>
   )
 }
